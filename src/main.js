2b46516215547a267cc48b1c4da08f2cc05deab5
xufei <411596701@qq.com>
2020-09-13 22:30:52 +0800

    对原声标签实现实dom

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-var a = <div />
+function createElement(target, attribute, ...children) {
+  const e = document.createElement(target)
+  for (const key in attribute) {
+    e.setAttribute(key, attribute[key])
+  }
+  for (const child of children) {
+    if (typeof child === 'string') {
+      e.appendChild(document.createTextNode(child))
+    } else {
+      e.appendChild(child)
+    }
+  }
+  return e
+}
+
+// 因为有jsx plugin，以下代码会转换为createElement的形式
+// 注意直接在document直接appendChild,`document.appendChild`是不对的
+document.body.appendChild(<div class="outer-div">
+  <span>child1</span>
+  <span id="inner-span">child2</span>
+  <span>child3</span>
+</div>)
